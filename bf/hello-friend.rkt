1f07af508f533a731d9a5482512b73c33a140620
Tom <antoniofdiass@gmail.com>
2019-12-10 13:38:38 -0300

    Add code examples
    Also starts the brainfvck-bf parser

---
+++ bf/hello-friend.rkt
@@ -0,0 +1,5 @@
+inc 6 begin fwd inc 12 rwd dec end fwd write
+fwd inc 10 begin fwd inc 10 rwd dec end fwd inc write
+inc 7 write write inc 3 write fwd inc 4 begin fwd inc 11 rwd dec end fwd write
+rwd inc 3 begin fwd dec 4 rwd dec end fwd write rwd 5 inc 3 begin fwd inc 5 rwd dec end fwd write
+fwd 2 write inc 3 write dec 6 write dec 8 write fwd 2 inc write
